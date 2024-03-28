32bbf1c78b965245c2c111d29c512b7ed2b344f0
Medi <csetemedard@gmail.com>
2024-03-28 11:30:51 +0100

    cleaning

--- dao/users-dao.js
+++ dao/users-dao.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql2/promise');
+const oracledb = require('oracledb');
 
 class UsersDAO {
     async createUsers(name, username, password){
-        const connection = await mysql.createConnection({host:'localhost',user:'root',database:'moodletest'})
+        const connection = await oracledb.createConnection({host:'localhost',user:'root',database:'moodletest'})
         await connection.query('INSERT INTO user (name, username, password) VALUES (?, ?, ?)', [name, username, password])
         connection.end();
         return;
@@ -10,33 +10,33 @@ class UsersDAO {
 
 
     async deleteUser(id){
-        const connection = await mysql.createConnection({host:'localhost',user:'root',database:'moodletest'})
+        const connection = await oracledb.createConnection({host:'localhost',user:'root',database:'moodletest'})
         await connection.query('DELETE FROM user WHERE id=?', [id])
         connection.end();
         return;
     }
     async getUsers(){
-        const connection = await mysql.createConnection({host:'localhost',user:'root',database:'moodletest'})
+        const connection = await oracledb.createConnection({host:'localhost',user:'root',database:'moodletest'})
         const [result, query]= await connection.execute('SELECT * FROM user');
         connection.end();
         return result;
     }
     async getUsersById(id) {
-        const connection = await mysql.createConnection({host:'localhost',user:'root',database:'moodletest'});
+        const connection = await oracledb.createConnection({host:'localhost',user:'root',database:'moodletest'});
         const query= await connection.execute('SELECT * FROM user WHERE id=? ', [id]);
         connection.end();
         return query[0][0];
     };
 
     async getUsersByUserName(username) {
-        const connection = await mysql.createConnection({host:'localhost',user:'root',database:'moodletest'});
+        const connection = await oracledb.createConnection({host:'localhost',user:'root',database:'moodletest'});
         const query= await connection.execute('SELECT * FROM user WHERE username = ? ', [username]);
         connection.end();
         return query[0][0];
     };
 
     async modifyUserRole(id, role) {
-        const connection = await mysql.createConnection({host:'localhost',user:'root',database:'moodletest'});
+        const connection = await oracledb.createConnection({host:'localhost',user:'root',database:'moodletest'});
         await connection.query('UPDATE user SET role=? WHERE id = ?', [role, id]);
         connection.end();
         return;
@@ -44,7 +44,7 @@ class UsersDAO {
 
 
     async changeUserLoggedin(username) {
-        const connection = await mysql.createConnection({host:'localhost',user:'root',database:'moodletest'});
+        const connection = await oracledb.createConnection({host:'localhost',user:'root',database:'moodletest'});
         await connection.query('UPDATE user SET loggedin=!loggedin WHERE username=?', [username]);
         connection.end();
         return;
