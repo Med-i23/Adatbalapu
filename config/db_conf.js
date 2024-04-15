f9bb022150b58e7d93ba1a67a318c47f54b82d0f
kekszharcos <b.enedek670@gmail.com>
2024-04-15 18:26:26 +0200

    hagyhatod a db_configba ezeket, ha a hsoddal belépsz meg azokkal amik ott vannak akkor müködik

--- config/db_conf.js
+++ config/db_conf.js
@@ -1,6 +1,6 @@
 module.exports={
-    username: 'C##KAZRXM',
-    password: 'TE_JELSZAVAD',
+    username: 'C##D3FUQL',
+    password: '2024Adatb',
     connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=9999))(CONNECT_DATA=(SERVER=DEDICATED)(SID=orania2)))"
 }
 
